069319fea0bfa713c8466beb81f37f198394325f
Dmitriy <svsdima1995@mail.ru>
2020-11-17 11:47:56 +0300

    Начинаем создавать новое приложение React + Redux .Создал каркас приложения

--- src/components/hoc/with-resto-service.js
+++ src/components/hoc/with-resto-service.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import RestoServiceContext from '../resto-service-context';
 
-const WithRestoService = () => () => {
-    return 1;
+
+/* Компонент высшего порядка - эту функция, которая будет возвращать функцию, которая как аргумент будет получать компонент, этому компоненту можем передать свойства */
+const WithRestoService = () => (Wrapped) => {
+    return (props) => {
+        return (
+            <RestoServiceContext.Consumer>
+                {
+                    (RestoService) => {
+                        return <Wrapped {...props} RestoService={RestoService} />
+                    } 
+                }
+            </RestoServiceContext.Consumer>
+        )
+    };
 };
 
 export default WithRestoService;
---
+++ src/services/resto-service.js
@@ -0,0 +1,5 @@
+export default class ReastoService {
+    getMenuItems() {
+        return [];
+    }
+}
